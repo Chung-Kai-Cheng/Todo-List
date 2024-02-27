30efa9200fbf548e645b731beb180809a91595a7
njdhdl1223 <a08tw04b05@gmail.com>
2024-02-27 23:01:35 +0800

    feat: add login funciton to login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,10 +8,27 @@ import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { login } from 'api/auth';
 
 const LoginPage = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  const handleClick = async () => {
+    //排除 username 或 password 長度為 0 的狀況
+    if (username.length === 0 || password.length === 0) {
+      return;
+    }
+    //取值
+    const { success, authToken } = await login({
+      username,
+      password,
+    });
+    //若成功則存入localStorage
+    if (success) {
+      localStorage.setItem('authToken', authToken);
+    }
+  };
   return (
     <AuthContainer>
       <div>
@@ -37,7 +54,7 @@ const LoginPage = () => {
           onChange={(passWordInputValue) => setPassword(passWordInputValue)}
         />
       </AuthInputContainer>
-      <AuthButton>登入</AuthButton>
+      <AuthButton onClick={handleClick}>登入</AuthButton>
       <Link to="/signup">
         <AuthLinkText>註冊</AuthLinkText>
       </Link>
