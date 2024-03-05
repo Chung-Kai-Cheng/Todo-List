40132a4e4c6f1daaa51baee98f22a42739f4493b
njdhdl1223 <a08tw04b05@gmail.com>
2024-03-05 23:36:44 +0800

    feat: export context hook

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { checkPermission, login, register } from 'api/auth';
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 import * as jwt from 'jsonwebtoken';
 import { useLocation } from 'react-router-dom';
 
@@ -12,6 +12,7 @@ const defaultAuthContext = {
 };
 const AuthContext = createContext(defaultAuthContext);
 
+export const useAuth = () => useContext(AuthContext);
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [payload, setPayload] = useState(null);
