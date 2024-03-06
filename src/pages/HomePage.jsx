b7a9a14b9446555f5421604a0ce9e48ab00eb222
njdhdl1223 <a08tw04b05@gmail.com>
2024-03-06 09:54:42 +0800

    feat: add home page navigation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,19 @@
+import { useAuth } from 'contexts/AuthContext';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+// 作為轉接站，透過 useEffect 再導向其他的頁面
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/todo');
+    } else {
+      navigate('/login');
+    }
+  }, [navigate, isAuthenticated]);
 };
 
 export default HomePage;
